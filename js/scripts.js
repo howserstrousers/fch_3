cda9dca671fe5f4849e8c34600b79f5528675423
howserstrousers <fritzcharleshowser@gmail.com>
2017-07-24 23:24:22 -0400

    s

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,22 @@ window.onload = function() {
 $('.skillsb').click(function(){
             $('.skills').toggle();
         });
+
 });
 }
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('smalls1'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '../img/smalls/smalls_sleeping.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('smalls2'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '../img/smalls/smalls_360.json'
+})
