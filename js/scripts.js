ff8e6affc5cb639e756a2d493e02611d9782f178
howserstrousers <fritzcharleshowser@gmail.com>
2017-10-09 17:30:04 -0400

    recent
    slide

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,5 @@ var animation = bodymovin.loadAnimation({
   renderer: 'svg',
   loop: true,
   autoplay: true,
-  path: '../img/smalls/smalls_sleeping.json'
-})
-
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('smalls2'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: '../img/smalls/smalls_360.json'
-})
+  path: '/img/smalls/smalls_sleeping.json'
+});
