294c43713aab0264f0f0bd00a1d6cb6d4f6c9461
howserstrousers <fritzcharleshowser@gmail.com>
2017-12-04 23:11:18 -0500

    new design pre slider

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,3 @@ $('.skillsb').click(function(){
 });
 }
 
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('smalls1'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: '/img/smalls/smalls_sleeping.json'
-});
