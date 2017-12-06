09bfe6111a322838738c05581047f7d8703ddc0c
howserstrousers <fritzcharleshowser@gmail.com>
2017-12-05 21:02:49 -0500

    slider functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 window.onload = function() {
-	$(document).ready(function() {
+$(document).ready(function() {
 
-$('.skillsb').click(function(){
-            $('.skills').toggle();
-        });
+	$('.skillsb').click(function(){
+	            $('.skills').toggle();
+	        });
+
+	$('.work_container').slick({
+		arrows:false,
+	});
+
+	$('.left').click(function(){
+	  $('.work_container').slick('slickPrev');
+	})
+
+	$('.right').click(function(){
+	  $('.work_container').slick('slickNext');
+	})
 
 });
 }
